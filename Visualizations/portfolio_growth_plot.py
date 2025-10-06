0b984570142fbd3319462cdcabe27822e734e4a9
FMDX-7 <99770413+FMDX-7@users.noreply.github.com>
2025-10-06 18:57:11 -0400

    Handle empty selected_factors in portfolio plot

--- Visualizations/portfolio_growth_plot.py
+++ Visualizations/portfolio_growth_plot.py
@@ -12,11 +12,10 @@ def plot_portfolio_growth(years, portfolio_values, selected_factors=None, restri
         factor_set_name (str): Name of the factor set for labeling
         restrict_fossil_fuels (bool): Whether fossil fuel companies were excluded
     """
-    
-    if selected_factors is None:
+     if selected_factors is None or len(selected_factors) == 0:
         factor_set_name = "Selected Factors"
     else:
-        factor_set_name = ", ".join(selected_factors)
+        factor_set_name = ", ".join(str(f) for f in selected_factors)
 
     plt.figure(figsize=(10, 6))
     plt.plot(years, portfolio_values, marker='o', linestyle='-', color='b', label=f'Portfolio ({factor_set_name})')
